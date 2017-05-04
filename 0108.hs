7c6d4697ae6beb8973f511f737e975ba814bff7b
Didier Pieroux <didier_pieroux@yahoo.com>
2017-05-05 01:13:00 +0200

    euler 180 preparation

---
+++ 0108.hs
@@ -0,0 +1,40 @@
+{-------------------------------------------------------------------------------
+
+In the following equation x, y, and n are positive integers.
+ 
+        1/x + 1/y = 1/n
+
+For n = 4 there are exactly three distinct solutions:
+
+        1/5 + 1/20 = 1/4
+        1/6 + 1/12 = 1/4
+        1/8 + 1/ 8 = 1/4
+
+What is the least value of n for which the number of distinct solutions exceeds
+one-thousand?
+
+--------------------------------------------------------------------------------
+
+Without any loss of generality, we limit ourselve to the case 1/x <= 1/y < 1/n,
+i.e. x >= y > n.
+
+In addition, if (x, y, n) is a solution, so is (ax, ay, an) for any a integer. 
+
+Let us decompose x in a y + b, with 1 <= a and 0 <= b < y. 
+
+It comes:
+
+        1/x + 1/y = (y + a y + b)/[y (a y + b)] = 1/n
+or,     
+        n = y (a y + b) / (y + a y + b)
+
+               /         y      \
+          = y | 1 - -----------  |
+               \    y (1+a) + b / 
+
+n and y being integer, y²/(y (1+a) + b) must also be integer.
+
+
+
+
+-------------------------------------------------------------------------------}
